93f19129c7d94eb979f643762c7e877cbd1aea4b
Stretch A. Bobbito <canikickit86@protonmail.com>
2020-02-09 21:44:45 -0600

    Passing data through props fix

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class Square extends React.Component {
   
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={1} />;
+        return <Square value={i} />;
     }
 
     render() {
